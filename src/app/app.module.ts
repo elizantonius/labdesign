a32c3fc13b68e2e6b6d3302fa8d76fc2eeb8b1e5
elizantonius <lonecode1777@gmail.com>
2022-06-24 07:47:13 +0700

    otwgas

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BerandaComponent } from './beranda/beranda.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BerandaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
